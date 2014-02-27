672786191977f452305e9ae14132ffaf69d724b3
Trevor Linton <trevor.linton@gmail.com>
2014-02-27 12:10:13 -0700

    Simplifying project structure/config

---
+++ config.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+export EMSCRIPTEN_ROOT=`em-config EMSCRIPTEN_ROOT`
+export NINJA_ROOT=`which ninja`
+ 
+if [ ! "$NINJA_ROOT" ]; then
+   echo "Cannot find ninja build tool.";
+   exit 1
+fi
+
+if [ -d "$EMSCRIPTEN_ROOT" ]; then
+  export EMSCRIPTEN_EMCC=$EMSCRIPTEN_ROOT/emcc
+  export EMSCRIPTEN_EMCPP=$EMSCRIPTEN_ROOT/em++
+  if [ -z "$EMSCRIPTEN_INCLUDE" ]; then
+     export EMSCRIPTEN_INCLUDE=$EMSCRIPTEN_ROOT/system/include
+  fi
+  if [ ! -d "$EMSCRIPTEN_INCLUDE" ]; then
+     echo "config.sh: Error: Cannot find system include directory for emscripten."
+     exit 1
+  fi
+else
+  echo "config.sh: Error: Cannot find root directory for emscripten."
+  exit 1
+fi
+
+if [ `uname`=='Darwin' ]; then
+  echo "Building Xcode and Ninja files."
+  export GYP_GENERATORS=xcode,ninja
+else
+ echo "Building Visual Studio and Ninja files."
+  export GYP_GENERATORS=msvs,ninja
+fi
+
+export GYP_DEFINES="clang=1"
+./tools/gyp-emscripten/gyp -Dsource_arch=js -Dtarget_arch=js -Darch=js --toplevel-dir=$PWD --depth=. --generator-output=. -G output_dir=. webkit.gyp
+
+if [ "$?" -ne "0" ]; then
+  echo "Failed to configure webkit.js properly."
+  exit 1
+else
+  echo "webkit.js project files successfully configured and built."
+fi
